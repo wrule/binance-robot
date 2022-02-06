f52dfc0c6840b045fdc7d028bcc54fe4a12c662f
jimao <jimao@jimaodeMBP.lan>
2022-02-06 17:38:54 +0800

    .gitignore

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,15 @@
 
 console.log('你好，世界');
+
+import Binance from 'node-binance-api';
+
+const binance = new Binance().options({
+  APIKEY: '<key>',
+  APISECRET: '<secret>'
+});
+
+async function main() {
+  console.info(await binance.futuresPrices());
+}
+
+main();
