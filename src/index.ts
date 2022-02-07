0ebdf169275d0e8b2a839201610cac73a20a31fd
jimao <jimao@jimaodeMacBook-Pro.local>
2022-02-07 21:42:50 +0800

    思考

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,24 @@ import { Keeper } from './keeper';
 
 const keeper = new Keeper();
 keeper.Start();
+
+function aaa(text: string) {
+  const execResult = /\d+/.exec(text);
+  if (execResult != null && execResult.length > 0) {
+    const num = Number(execResult[0]);
+    if (text.endsWith('m')) {
+      return num * 60;
+    } else if (text.endsWith('h')) {
+      return num * 60 * 60;
+    } else if (text.endsWith('d')) {
+      return num * 60 * 60 * 24;
+    } else if (text.endsWith('w')) {
+      return num * 60 * 60 * 24 * 7;
+    } else if (text.endsWith('M')) {
+      throw new Error('a');
+    }
+  }
+  throw new Error('b');
+}
+
+console.log(aaa('8w'));
