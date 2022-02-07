18f552401b4a89849219de9cdaaf66f7135c3ab0
jimao <jimao@jimaodeMacBook-Pro.local>
2022-02-07 17:28:10 +0800

    提交

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import Binance from 'node-binance-api';
 import secretKey from './.secret.json';
+import moment from 'moment';
 
 const binance = new Binance().options({
   APIKEY: secretKey.APIKEY,
@@ -9,10 +10,20 @@ const binance = new Binance().options({
 
 async function main() {
   console.log('程序运行...');
-  // await binance.useServerTime();
   let ticker = await binance.prices();
-  console.info(`Price of BNB: ${ticker.BNBUSDT}`);
-  // console.info(await binance.futuresPrices());
+  console.info('LINK/USDT价格: ', ticker.LINKUSDT);
+  // binance.buy('LINKUSDT', 1, 13);
+  // console.log('已经下限价单');
+
+  binance.websockets.chart('BTCUSDT', '30m', (symbol: any, interval: any, chart: any) => {
+    const list = Object.entries(chart).map(([time, data]) => ({
+      time: moment(new Date(Number(time))).format('YYYY-MM-DD HH:mm:ss'),
+      ...(data as any),
+    }));
+    console.log(list[list.length - 2]);
+  });
+
+  // console.log('运行结束...');
 }
 
 main();
